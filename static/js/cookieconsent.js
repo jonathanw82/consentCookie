f2a247c0e6166a279b7911f8abb3c3ea759a1c4b
JonathanW82 <Jonwheway@live.co.uk>
2020-11-17 13:53:30 +0000

    made this into a flask app as unless a server is running chrome and other browsers will not save cookies

---
+++ static/js/cookieconsent.js
@@ -0,0 +1,77 @@
+/*
+This code creates a cookie consent to run a modal that askes the user if they consent to cookies;
+*/
+
+const cookieStorage = {
+    // get an item from a cookie 
+    getItem: (key) =>{
+        // get a list of cookies and place them in a object with the cookie 
+        // key being the key and value being the value assigned to each key.
+        const cookies = document.cookie
+            // Split the string that comes back by a semicolon as thats the delimiter
+            // that all the cookies are broken up in the cookie string with.
+            .split(';')
+            // Map all the cookies in to an array so split them again, in to key value pairs
+            // and user the '=' because thata how they appear in the actual cookie string itself.
+            .map(cookie => cookie.split('='))
+            // Reduce the array so that every item that comes back in the subarray will be representative 
+            // of the key and value. Then recreate an object to spread back into the accumulator at each
+            // iteration, then for each key trim any whitespace off of it, the value is the value.
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+        return cookies[key]
+    },
+    // Set a cookie in the browser with key value pairs as parameters.
+    setItem: (key, value) =>{
+        document.cookie = `${key}=${value}`;
+    },
+};
+
+// Setting the localstorage holds the consent of the user so it only shows once.
+const storageType = cookieStorage;
+const consentPropertyName = 'gdpr_consent';
+
+// When we reference the local storage, if the key name is present or a value is associated with that key name
+// then this will return false, so do not show any popups. If it returns a false value and there is no value
+// associated with with it then return true and show the popup.
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+
+// If the user gives consent then this will set the consentPropertyName to True so the shouldshowPopup
+// will give us the result we need.
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () =>{
+    if (shouldShowPopup()) {
+        const consent = confirm('Agree to the trems and conditions of the site?');
+        if(consent){
+            saveToStorage();
+        }
+    }
+};
+
+
+
+/*
+This code demonstarights how to save things in to local or session storage.
+*/
+
+// // Setting the localstorage holds the consent of the user so it only shows once.
+// const storageType = sessionStorage;
+// const consentPropertyName = 'gdpr_consent';
+
+// // When we reference the local storage, if the key name is present or a value is associated with that key name
+// // then this will return false, so do not show any popups. If it returns a false value and there is no value
+// // associated with with it then return true and show the popup.
+// const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+
+// // If the user gives consent then this will set the consentPropertyName to True so the shouldshowPopup
+// // will give us the result we need.
+// const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+// window.onload = () =>{
+//     if (shouldShowPopup()) {
+//         const consent = confirm('Agree to the trems and conditions of the site?');
+//         if(consent){
+//             saveToStorage();
+//         }
+//     }
+// };
